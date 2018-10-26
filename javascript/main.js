5387582d6f2c75403997d2733f9a8c88845a15c0
Blake M <Blake.manookin@gmail.com>
2018-10-25 22:12:56 -0600

    cleaned up for final submission

--- javascript/main.js
+++ javascript/main.js
@@ -1,15 +1,10 @@
 $('.minput').on('keydown', function (event) {
-    // console.log(event.which);
     if (event.which === 13) {
         additem();
         newList()
     }
 });
-// $('.nameBar').sortable({
-//     handle: '.handle'
-// });//add edit button
-//$('.row').addClass('greyText');
-//$('.contain').find('p').css('color','blue');
+
 $('.checked').click(function () {
 
     $('.checkedRow').animate({
@@ -35,10 +30,8 @@ function newList() {
             '<div class="nameBar">' +
             '<h1 class="listName" contenteditable="true">' + myListName + '</h1>' +
 
-            // '<button onclick="removeList()">delete</button> </div>' +
             '<input class="minput" type="text" placeholder="List Item Here">' +
             '<button onclick="clearlist(this)">Clear the List</button>' +
-            // '<button class="checked">Clear Checked items</button>' +
             '<i onclick="additem(this)" class="additem far fa-plus-square fa-lg"></i>' +
             '<i class="kill fas fa-bomb fa-lg"></i>' +
             '<div class="contain">' +
@@ -53,52 +46,10 @@ function newList() {
             }, 800, function () {
                 $(this).parent().remove();
             });
-            //$(this).remove();
+
 
         });
-        // function additem () {
-        //     let myname = $('.minput').val();
-        //     console.log(myname);
-        //     $(this).parent().append('<div class="row">' +
-        //         '<span contenteditable="true">' + myname + '</span>' +
-        //         '<i class="box far fa-square"></i><i class=" trash fas fa-ban"></i> ' +
-        //         '<i class=" handle fas fa-sort"></i></div></div>'
-        //     );
-        //     $('.minput').val('');
-        //
-        //
-        //     $('.trash').click(function () {
-        //
-        //         $(this).parent().parent().animate({
-        //             opacity: 0,
-        //             left: "+=50"
-        //         }, 800, function () {
-        //             $(this).remove();
-        //         });
-        //
-        //     });
-        //     $('.box').click(function () {
-        //
-        //         $(this).parent().addClass('checkedRow');
-        //         $(this).replaceWith('<i class="checkBox far fa-check-square"></i>')
-        //     });
-        //     $('.checkBox').click(function () {
-        //
-        //         $(this).parent().removeClass('checkedRow');
-        //         $(this).replaceWith('<i class="box far fa-square"></i>')
-        //     })
-        //
-        // }
-        // $('.checked').click(function () {
-        //
-        //      $('.checkedRow').animate({
-        //           opacity: 0,
-        //              left: "+=50"
-        //          }, 800, function () {
-        //                   $('.checkedRow').remove()
-        //              });
-        //
-        //     });
+
     }
 
 
@@ -122,7 +73,6 @@ function additem(element) {
         '<span contenteditable="true">' + myname + '</span>' +
         '<i onclick="check(this)" class="far fa-square"></i><i class=" trash fas fa-ban"></i> ' +
         '</div>'
-        // <i class=" handle fas fa-sort"></i>
     );
     $('.minput').val('');
     let numChildren = $('.contain').children().length;
@@ -140,21 +90,18 @@ function additem(element) {
     });
 
 
-
 }
+
 function check(element) {
 
     $(element).parent().addClass('checkedRow');
     $(element).replaceWith('<i onclick="uncheck(this)" class="far fa-check-square"></i>')
 }
+
 function uncheck(element) {
 
     $(element).parent().removeClass('checkedRow');
     $(element).replaceWith('<i onclick="check(this)" class="far fa-square"></i>')
 }
 
-//create array to hold lists
 
-//create an object that holds an array of tasks
-
-//
